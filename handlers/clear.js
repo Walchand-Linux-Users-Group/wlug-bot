64739b115f85b440de32f19d6ad54646c5d5b51d
Suyash <suyashc222@gmail.com>
2021-07-15 00:31:29 +0530

    Bug fixed

--- handlers/clear.js
+++ handlers/clear.js
@@ -1,15 +1,16 @@
-function handleClear(message,number) {
-    try{
-        number = parseInt(number)
+function handleClear(message, number) {
+    try {
+        number = parseInt(number) + 1
     }
-    catch(err){
+    catch (err) {
         message.channel.send("Invalid number!")
+        return
     }
 
-    if(number>100)
-        number = 100
+    if (number > 100)
+        number = 101
 
-    if(number<1)
+    if (number < 1)
         number = 1
 
     message.channel.bulkDelete(number);
